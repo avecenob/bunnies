c51396e9d64370e4e0277b993c2c79635bc152f1
avecenob <aryabasc@gmail.com>
2025-06-29 18:19:46 +0700

    update app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,8 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './users/user.entity';
 import { OrdersModule } from './orders/orders.module';
 import { ProductsModule } from './products/products.module';
-import { Product } from './products/products.entity';
-import { Category } from './products/category.entity';
 import { AppDataSource } from './data-source';
 
 @Module({
