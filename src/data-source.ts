d2dd00f4decf7d6b2413763931edebd89164bf6f
avecenob <aryabasc@gmail.com>
2025-06-30 08:46:28 +0700

    update use dotenv

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,11 +3,13 @@ import { config } from './config';
 import { User } from './users/user.entity';
 import { Product } from './products/products.entity';
 import { Category } from './products/category.entity';
+import { Order } from './orders/orders.entity';
+import { OrderItem } from './orders/order-items.entity';
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
   ...config.db,
   synchronize: true,
-  logging: true,
-  entities: [User, Product, Category],
+  // logging: true,
+  entities: [User, Product, Category, Order, OrderItem],
 });
