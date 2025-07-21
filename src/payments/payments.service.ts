1f8086fe8fd40b51ecccf3f89a5f6fe07d85bddb
avecenob <aryabasc@gmail.com>
2025-07-21 23:52:44 +0700

    fix bug

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,23 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { OrdersService } from 'src/orders/orders.service';
-import { UsersService } from 'src/users/users.service';
 // import Midtrans from 'midtrans-client';
 
 @Injectable()
-export class PaymentsService {
-  // private snap = new Midtrans.snap({
-  //   isProduction: false,
-  //   serverKey: 'SB-Mid-server-bReOJkzNRdjtb4W7GoMZmAGb',
-  //   clientKey: 'SB-Mid-client-GDolMZNgGeeGe4BY',
-  // });
-  // constructor(
-  //   private ordersService: OrdersService,
-  //   private usersService: UsersService,
-}
-
-/**
- * TODO
- *
- * Integrate with Midtrans
- *
- */
+export class PaymentsService {}
